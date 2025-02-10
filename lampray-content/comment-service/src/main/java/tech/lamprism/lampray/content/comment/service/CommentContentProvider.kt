012846a8baf5bb013164ab47945833f0bdd7cf09
RollW <73376341+Roll-W@users.noreply.github.com>
2025-02-10 22:17:29 +0800

    feat(comment): implementation of `ContentOperator` and provider.
    Add comment implementation of `ContentProvider` and `ContentOperator`.

---
+++ lampray-content/comment-service/src/main/java/tech/lamprism/lampray/content/comment/service/CommentContentProvider.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2023 RollW
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.lamprism.lampray.content.comment.service
+
+import org.springframework.stereotype.Service
+import tech.lamprism.lampray.content.ContentOperator
+import tech.lamprism.lampray.content.ContentProvider
+import tech.lamprism.lampray.content.ContentTrait
+import tech.lamprism.lampray.content.ContentType
+import tech.lamprism.lampray.content.comment.Comment
+import tech.lamprism.lampray.content.comment.persistence.CommentDo.Companion.toDo
+import tech.lamprism.lampray.content.comment.persistence.CommentRepository
+import tech.lamprism.lampray.content.common.ContentErrorCode
+import tech.lamprism.lampray.content.common.ContentException
+import tech.lamprism.lampray.content.service.ContentMetadataService
+
+/**
+ * @author RollW
+ */
+@Service
+class CommentContentProvider(
+    private val commentRepository: CommentRepository,
+    override val contentMetadataService: ContentMetadataService
+) : ContentProvider, CommentOperatorDelegate {
+    override fun supports(contentType: ContentType): Boolean {
+        return contentType == ContentType.COMMENT
+    }
+
+    override fun getContentOperator(
+        contentTrait: ContentTrait,
+        checkDelete: Boolean
+    ): ContentOperator {
+        val commentDo = commentRepository
+            .findById(contentTrait.contentId)
+            .orElseThrow {
+                ContentException(ContentErrorCode.ERROR_CONTENT_NOT_FOUND, "Comment not found")
+            }
+        return CommentOperatorImpl(commentDo.lock(), this, checkDelete)
+    }
+
+    override fun updateComment(comment: Comment) {
+        commentRepository.save(comment.toDo())
+    }
+}
---
+++ lampray-content/comment-service/src/main/java/tech/lamprism/lampray/content/comment/service/CommentOperatorImpl.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2023 RollW
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tech.lamprism.lampray.content.comment.service
+
+import tech.lamprism.lampray.content.ContentDetails
+import tech.lamprism.lampray.content.ContentDetailsMetadata
+import tech.lamprism.lampray.content.ContentOperator
+import tech.lamprism.lampray.content.comment.Comment
+import tech.lamprism.lampray.content.service.AbstractContentOperator
+import tech.rollw.common.web.CommonRuntimeException
+import java.time.OffsetDateTime
+
+/**
+ * @author RollW
+ */
+class CommentOperatorImpl internal constructor(
+    private var comment: Comment,
+    private val delegate: CommentOperatorDelegate,
+    checkDeleted: Boolean
+) : AbstractContentOperator(comment, delegate.contentMetadataService, checkDeleted),
+    ContentOperator {
+
+    private val commentBuilder: Comment.Builder = comment.toBuilder()
+
+    override fun updateContent(): ContentDetails {
+        comment = commentBuilder
+            .setUpdateTime(OffsetDateTime.now())
+            .build()
+        delegate.updateComment(comment)
+        return comment
+    }
+
+    @Throws(CommonRuntimeException::class)
+    override fun setNameInternal(name: String): Boolean {
+        throw UnsupportedOperationException()
+    }
+
+    @Throws(CommonRuntimeException::class)
+    override fun setContentInternal(content: String): Boolean {
+        throw UnsupportedOperationException()
+    }
+
+    @Throws(CommonRuntimeException::class)
+    override fun setMetadataInternal(metadata: ContentDetailsMetadata): Boolean {
+        return false
+    }
+}
