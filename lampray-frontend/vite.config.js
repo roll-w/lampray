a3301a89707fc88f028b5e23093676b2e1dd121c
RollW <73376341+Roll-W@users.noreply.github.com>
2025-03-06 21:24:01 +0800

    build(frontend): reduce the build output size by manual chunks.

--- lampray-frontend/vite.config.js
+++ lampray-frontend/vite.config.js
@@ -14,22 +14,37 @@
  * limitations under the License.
  */
 
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 5000,
-    open: true,
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'vue': 'vue/dist/vue.esm-bundler.js'
+    plugins: [vue()],
+    server: {
+        host: '0.0.0.0',
+        port: 5000,
+        open: true,
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            'vue': 'vue/dist/vue.esm-bundler.js'
+        }
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString()
+                            .split('node_modules/')[1]
+                            .split('/')[0]
+                            .toString();
+                    }
+                }
+            }
+        }
     }
-  }
 })
