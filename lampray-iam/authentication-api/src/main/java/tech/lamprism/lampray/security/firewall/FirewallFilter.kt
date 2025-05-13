0e30db261877c775e15c6d8fd992bbc377b96f0a
RollW <73376341+roll-w@users.noreply.github.com>
2025-05-13 21:17:45 +0800

    feat(auth): add `FirewallFilter` implementation.

---
+++ lampray-iam/authentication-api/src/main/java/tech/lamprism/lampray/security/firewall/FirewallFilter.kt
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2023-2025 RollW
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.lamprism.lampray.security.firewall
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.web.filter.OncePerRequestFilter
+import tech.lamprism.lampray.web.common.ApiContext
+import tech.lamprism.lampray.web.common.ApiContextAware
+import tech.rollw.common.web.system.ContextThreadAware
+
+/**
+ * @author RollW
+ */
+class FirewallFilter(
+    firewalls: List<Firewall>,
+) : OncePerRequestFilter(), ApiContextAware {
+    private val firewalls = firewalls
+        .toSortedSet { o1, o2 ->
+            o1.priority.compareTo(o2.priority)
+        }
+
+    fun addFirewall(firewall: Firewall) {
+        firewalls.add(firewall)
+    }
+
+    fun removeFirewall(firewall: Firewall) {
+        firewalls.remove(firewall)
+    }
+
+    override fun getFilterName() = "FirewallFilter"
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val user = apiContextAware.contextThread.context.user
+        val firewallAccessRequest = HttpFirewallAccessRequest(request, user)
+        for (firewall in firewalls) {
+            val accessResult = firewall.verifyRequest(firewallAccessRequest)
+            when (accessResult.case) {
+                FirewallAccessResult.Case.ALLOW -> {
+                    filterChain.doFilter(request, response)
+                    return
+                }
+
+                FirewallAccessResult.Case.DENY -> {
+                    throw FirewallException("Access denied: ${accessResult.message}")
+                }
+
+                FirewallAccessResult.Case.NEUTRAL -> {
+                    continue
+                }
+            }
+        }
+        filterChain.doFilter(request, response)
+    }
+
+    private lateinit var apiContextAware: ContextThreadAware<ApiContext>
+
+    override fun setApiContext(contextAware: ContextThreadAware<ApiContext>) {
+        this.apiContextAware = contextAware
+    }
+}
