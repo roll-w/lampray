b9558a6e0cea4a6cf294857f19bd1cc335de3607
RollW <73376341+roll-w@users.noreply.github.com>
2025-07-15 21:37:35 +0800

    feat(security): add FirewallConfiguration for managing firewall filters.

---
+++ lampray-iam/authentication-service/src/main/java/tech/lamprism/lampray/security/firewall/FirewallConfiguration.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2023-2025 RollW
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.lamprism.lampray.security.firewall
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import tech.lamprism.lampray.security.firewall.filtertable.FileFilterTable
+import tech.lamprism.lampray.security.firewall.filtertable.FilterTable
+import tech.lamprism.lampray.security.firewall.filtertable.FilterTableFirewall
+
+/**
+ * @author RollW
+ */
+@Configuration
+class FirewallConfiguration {
+
+    @Bean
+    fun firewallFilter(firewalls: List<Firewall>): FirewallFilter {
+        return FirewallFilter(firewalls)
+    }
+
+    @Bean
+    fun filterTableFirewall(filterTable: FilterTable): Firewall {
+        return FilterTableFirewall(filterTable)
+    }
+
+    @Bean
+    fun filterTable(): FilterTable {
+        // TODO: Make this configurable
+        return FileFilterTable("filtertable.txt")
+    }
+}
