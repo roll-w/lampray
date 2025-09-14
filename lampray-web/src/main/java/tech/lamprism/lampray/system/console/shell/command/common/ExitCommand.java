9277f4485bf2ff726767280710ab05478582c190
rollw <73376341+roll-w@users.noreply.github.com>
2025-09-14 14:47:56 +0800

    feat(shell): add exit and shutdown commands for shell console

---
+++ lampray-web/src/main/java/tech/lamprism/lampray/system/console/shell/command/common/ExitCommand.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2023-2025 RollW
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.lamprism.lampray.system.console.shell.command.common;
+
+import org.springframework.shell.ExitRequest;
+import org.springframework.shell.command.annotation.Command;
+import org.springframework.shell.standard.AbstractShellComponent;
+import org.springframework.stereotype.Component;
+import tech.lamprism.lampray.system.console.CommandGroups;
+
+/**
+ * @author RollW
+ */
+@Component
+@Command(command = "exit", description = "Exit current shell, but does not stop the application",
+        group = CommandGroups.COMMON)
+public class ExitCommand extends AbstractShellComponent {
+    @Command
+    public void exit() {
+        throw new ExitRequest();
+    }
+}
