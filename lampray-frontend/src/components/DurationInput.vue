d8278833feb52bcf2c192eb0e4692ce2d8058475
rollw <73376341+roll-w@users.noreply.github.com>
2025-12-16 00:29:26 +0800

    chore: code polish

--- lampray-frontend/src/components/DurationInput.vue
+++ lampray-frontend/src/components/DurationInput.vue
@@ -18,10 +18,6 @@
 import {computed, ref, watch} from "vue"
 import {useI18n} from "vue-i18n"
 
-/**
- * Duration input component
- * @author RollW
- */
 interface Props {
     modelValue?: number  // Total seconds
     placeholder?: string
